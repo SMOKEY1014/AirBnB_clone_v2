51bab31c555556518599d00091d1d827b83ba07c
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-04-15 11:57:06 +0200

    database created but Password is different from the one provided, reason : password given does not satisfy the current policy requirements

---
+++ update_password.sql
@@ -0,0 +1,3 @@
+ALTER USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+UPDATE mysql.user SET authentication_string = PASSWORD('hbnb_dev_pwd') WHERE user = 'hbnb_dev' AND host = 'localhost';
+FLUSH PRIVILEGES;
