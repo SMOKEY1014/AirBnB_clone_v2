38ab8fe1aea0e36efff73b975d7a1581c899fccd
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-04-15 12:00:49 +0200

    adding a test db and test user

--- update_password.sql
+++ update_password.sql
@@ -1,3 +1,5 @@
 ALTER USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 UPDATE mysql.user SET authentication_string = PASSWORD('hbnb_dev_pwd') WHERE user = 'hbnb_dev' AND host = 'localhost';
 FLUSH PRIVILEGES;
+
+ALTER USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'Pass@123' REPLACE 'hbnb_dev_pwd';
