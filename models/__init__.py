55098c3679a79b3605236c52bcaffeb74ffd79d9
SMOKEY1014 <smokeydhlamini@gmail.com>
2024-04-16 14:57:54 +0200

    task 6

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""
+module instantiates an object of class DBStorage
+if the environment variable HBNB_TYPE_STORAGE is equal to db or
+This module instantiates an object of class Storage
+"""
 from models.engine.file_storage import FileStorage
+from models.engine.db_storage import DBStorage
+from os import getenv
 
-
-storage = FileStorage()
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    storage = DBStorage()
+else:
+    storage = FileStorage()
 storage.reload()
--- models/state.py
+++ models/state.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    name = Column(String(128), nullable=False)
+    __tablename__ = "states"
